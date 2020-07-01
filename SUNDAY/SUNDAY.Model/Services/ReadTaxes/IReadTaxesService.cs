b665a564a5290ea50cdf63d29d0c2e79867a919b
jurciusmantas <jurciusmantas@gmail.com>
2020-07-01 18:34:20 +0300

    init commit with async controller and first service

---
+++ SUNDAY/SUNDAY.Model/Services/ReadTaxes/IReadTaxesService.cs
@@ -0,0 +1,11 @@
+﻿using SUNDAY.Model.DAO;
+using System;
+using System.Threading.Tasks;
+
+namespace SUNDAY.Model.Services.ReadTaxes
+{
+    public interface IReadTaxesService
+    {
+        Tax GetTaxAsync(string municipality, DateTime date);
+    }
+}
